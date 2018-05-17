e60580ec5971b4a3d1cf3530c29b5728d31718c2
alfoqahaah@gmail.com <alfoqahaah@gmail.com>
2018-05-17 08:32:39 +0300

    Make the heroku put its runtime port

--- app/server.js
+++ app/server.js
@@ -7,7 +7,8 @@ app.use(express.static('public'));
 /*Use Offers API as a middleware to save the modulerity*/
 app.use(offers);
 
-const port = 3005;
+var port = process.env.PORT || 8000
+
 app.listen(port, function(){
 	console.log('Listening on port ' + port);
 });
